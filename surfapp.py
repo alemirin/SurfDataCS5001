47e08dd57dde1af7a5f385e17580961f6622f3e3
alemirin <138611402+alemirin@users.noreply.github.com>
2023-12-11 14:07:03 -0400

    Update surfapp.py

--- surfapp.py
+++ surfapp.py
@@ -3,15 +3,27 @@ from buoydatareader import *
 
 app = Flask(__name__)
 
-ocean_data = Waves()
-wave_data = ocean_data.get_wave_heights()
-period_data = ocean_data.get_wave_periods()
-wave_direction = ocean_data.get_wave_directions()
 
-wind_data = Winds()
-wind_speed = wind_data.get_wind_speed()
-wind_gusts = wind_data.get_gusts()
-wind_direction = wind_data.get_wind_direction()
+
+arecibo_wave_data = Waves('https://www.ndbc.noaa.gov/data/realtime2/41121.txt')
+arecibo_wave_conditions = [arecibo_wave_data.get_wave_heights(),
+                           arecibo_wave_data.get_wave_periods(),
+                           arecibo_wave_data.get_wave_direction()]
+
+arecibo_wind_data = Winds('https://www.ndbc.noaa.gov/data/realtime2/41121.txt')
+arecibo_wind_conditions = [arecibo_wind_data.get_wind_speed(),
+                           arecibo_wind_data.get_gusts(),
+                           arecibo_wind_data.get_wind_direction()]
+
+sanjuan_wave_data = Waves('https://www.ndbc.noaa.gov/data/realtime2/41053.txt')
+sanjuan_wave_conditions = [sanjuan_wave_data.get_wave_heights(),
+                           sanjuan_wave_data.get_wave_periods(),
+                           sanjuan_wave_data.get_wave_direction()]
+
+sanjuan_wind_data = Winds('https://www.ndbc.noaa.gov/data/realtime2/41053.txt')
+sanjuan_wind_conditions = [sanjuan_wind_data.get_wind_speed(),
+                           sanjuan_wind_data.get_gusts(),
+                           sanjuan_wind_data.get_wind_direction()]
 
 
 @app.route("/")
@@ -19,13 +31,14 @@ wind_direction = wind_data.get_wind_direction()
 @app.route("/arecibo")
 def home():
     return render_template('home.html',
-                           wave_data=wave_data,
-                           period_data=period_data,
-                           wave_direction=wave_direction,
-                           wind_speed=wind_speed,
-                           wind_gusts=wind_gusts,
-                           wind_direction=wind_direction)
+                           arecibo_wave_conditions=arecibo_wave_conditions,
+                           arecibo_wind_conditions=arecibo_wind_conditions)
 
+@app.route("/sanjuan")
+def sanjuan():
+    return render_template('sanjuan.html',
+                           sanjuan_wave_conditions=sanjuan_wave_conditions,
+                           sanjuan_wind_conditions=sanjuan_wind_conditions)
 
 if __name__ == '__main__':
     app.run(debug=True)
