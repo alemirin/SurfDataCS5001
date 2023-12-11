2906aa35c60144e63796a42f0c8a58077c8fa2e8
alemirin <138611402+alemirin@users.noreply.github.com>
2023-12-11 02:00:50 -0400

    Update buoydatareader.py
    Added urllib functionality so that the data is pulled directly from the National Data Buoy Center when the program is run. This gives real time latest data for surf conditions.

--- buoydatareader.py
+++ buoydatareader.py
@@ -1,78 +1,60 @@
-'''
+"""
 Final Project: Surf App
 Student: Alejandro Miranda
 Class: CS5001 Fall 2023
-'''
-
+"""
+from urllib.request import urlopen
 
 
 class Waves:
-    def __init__(self, file):
+    def __init__(self):
 
-        filename = file
-
-        data = open(filename, "r")
+        data = urlopen('https://www.ndbc.noaa.gov/data/realtime2/41121.txt')
 
         next(data)
         line_string2 = str(data.readline())
         if '#yr' in line_string2:
             units_list = line_string2.split()
-            self.height_units = units_list[6]
-            self.period_units = units_list[7]
+            self.height_units = units_list[8]
+            self.period_units = units_list[9]
             self.direction_units = 'degrees'
         line_string3 = str(data.readline())
         data_list = line_string3.split()
-        self.swell_height = float(data_list[6])
-        self.swell_period = float(data_list[7])
-        self.swell_direction_general = data_list[10]
-        self.swell_direction_specific = data_list[-1]
-        self.wind_swell_height = float(data_list[8])
-        self.wind_swell_period = float(data_list[9])
-        self.wind_swell_direction = data_list[11]
-        self.overall_wave_height = float(data_list[5])
+        self.swell_height = float(data_list[8])
+        self.swell_period = float(data_list[9])
+        self.swell_direction = int(data_list[11])
 
         data.close()
 
     def __str__(self):
-        conditions = ['wind swell height: ' + str(self.wind_swell_height) + ' ' + self.height_units,
-                'wind swell period: ' + str(self.wind_swell_period) + ' ' + self.period_units,
-                'wind swell direction: ' + self.wind_swell_direction,
+        conditions = [
                 'swell height: ' + str(self.swell_height) + ' ' + self.height_units,
                 'swell period: ' + str(self.swell_period) + ' ' + self.period_units,
-                'swell direction: ' + self.swell_direction_general,
-                'overall wave height: ' + str(self.overall_wave_height) + ' ' + self.height_units,
-                'average wave direction: ' + str(self.swell_direction_specific) + ' ' + self.direction_units]
+                'swell direction: ' + self.swell_direction_general]
         for item in conditions:
             print(str(item))
         return 'Data list: ' + str(conditions)
 
-
     def get_wave_heights(self):
-        return [self.wind_swell_height,
-                self.swell_height,
-                self.overall_wave_height]
+        return self.swell_height
 
     def get_wave_periods(self):
-        return [self.wind_swell_period,
-                self.swell_period]
+        return self.swell_period
 
     def get_wave_directions(self):
-        return [self.wind_swell_direction,
-                self.swell_direction_general,
-                self.swell_direction_specific]
+        return self.swell_direction
+
 
 class Winds:
-    def __init__(self, file):
+    def __init__(self):
 
-        filename = file
-
-        data = open(filename, "r")
+        data = urlopen('https://www.ndbc.noaa.gov/data/realtime2/41121.txt')
 
         next(data)
         line_string2 = str(data.readline())
         if '#yr' in line_string2:
             units_list = line_string2.split()
-            self.windspeed_units = units_list[6]
+            self.wind_speed_units = units_list[6]
             self.gust_units = units_list[7]
             self.direction_units = 'degrees'
         line_string3 = str(data.readline())
@@ -86,18 +68,17 @@ class Winds:
         else:
             self.gusts = 'Missing data from National Data Buoy Center.'
         if data_list[5] != "MM":
-            self.wind_direction = data_list[5]
+            self.wind_direction = int(data_list[5])
         else:
             self.wind_direction = 'Missing data from National Data Buoy Center.'
-
 
         data.close()
 
     def __str__(self):
         if isinstance(self.wind_speed, float) and isinstance(self.gusts, float):
             conditions = ['wind speed: ' + str(self.wind_speed) + ' ' + self.windspeed_units,
-                'wind gusts: ' + str(self.gusts) + ' ' + self.period_units,
-                'wind direction: ' + self.wind_direction]
+                          'wind gusts: ' + str(self.gusts) + ' ' + self.period_units,
+                          'wind direction: ' + self.wind_direction]
         elif isinstance(self.wind_speed, float) and isinstance(self.gusts, str):
             conditions = ['wind speed: ' + str(self.wind_speed) + ' ' + self.windspeed_units,
                           'wind gusts: ' + str(self.gusts),
@@ -114,10 +95,9 @@ class Winds:
             print(str(item))
         return 'Data list: ' + str(conditions)
 
-
     def get_wind_speed(self):
         if isinstance(self.wind_speed, float):
-            return str(self.wind_speed) + f'{self.windspeed_units}'
+            return str(self.wind_speed) + f'{self.wind_speed_units}'
         else:
             return self.wind_speed
 
@@ -128,4 +108,7 @@ class Winds:
             return self.gusts
 
     def get_wind_direction(self):
-        return self.wind_direction
+        if isinstance(self.wind_direction, int):
+            return str(self.wind_direction) + f'{self.direction_units}'
+        else:
+            return self.wind_direction
