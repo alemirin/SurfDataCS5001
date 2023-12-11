63a0611427ff6e36a0883172f6c4318554ec1258
alemirin <138611402+alemirin@users.noreply.github.com>
2023-12-11 01:58:28 -0400

    Update surfapp.py
    Modified code since buoydatareader.py file now pulls relevant data directly from the National Data Buoy Center website when the webapp is opened.

--- surfapp.py
+++ surfapp.py
@@ -3,30 +3,28 @@ from buoydatareader import *
 
 app = Flask(__name__)
 
-ocean_data = Waves('buoydata.txt')
+ocean_data = Waves()
 wave_data = ocean_data.get_wave_heights()
 period_data = ocean_data.get_wave_periods()
-direction_data = ocean_data.get_wave_directions()
+wave_direction = ocean_data.get_wave_directions()
 
-wind_data = Winds('winddata.txt')
+wind_data = Winds()
 wind_speed = wind_data.get_wind_speed()
 wind_gusts = wind_data.get_gusts()
 wind_direction = wind_data.get_wind_direction()
 
 
-
 @app.route("/")
 @app.route("/home")
+@app.route("/arecibo")
 def home():
     return render_template('home.html',
                            wave_data=wave_data,
                            period_data=period_data,
-                           direction_data=direction_data,
+                           wave_direction=wave_direction,
                            wind_speed=wind_speed,
                            wind_gusts=wind_gusts,
                            wind_direction=wind_direction)
-
-
 
 
 if __name__ == '__main__':
