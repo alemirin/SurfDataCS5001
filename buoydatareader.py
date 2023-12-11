ae0ef792b0fe9a15567607896108b3c6a3ed70ec
alemirin <138611402+alemirin@users.noreply.github.com>
2023-12-11 02:23:04 -0400

    Update buoydatareader.py
    Added docstrings to explain how the functions work clearly

--- buoydatareader.py
+++ buoydatareader.py
@@ -8,6 +8,15 @@ from urllib.request import urlopen
 
 class Waves:
     def __init__(self):
+        """
+        Initializing the class automatically routes to the webpage for the Arecibo NOAA buoy data and pulls
+        the information from the webpage text relevant to wave height, wave period and direction.
+
+        Args:
+            None.
+        Returns:
+            None, but assigns the values from the webpage to variables within the Waves class.
+        """
 
         data = urlopen('https://www.ndbc.noaa.gov/data/realtime2/41121.txt')
 
@@ -36,17 +45,54 @@ class Waves:
         return 'Data list: ' + str(conditions)
 
     def get_wave_heights(self):
+        """
+        Returns the wave height in meters.
+
+        Args:
+            None.
+        Returns:
+            self.swell_height (float): Swell height in meters
+        """
         return self.swell_height
 
     def get_wave_periods(self):
+        """
+        Returns the wave period in seconds.
+
+        Args:
+            None.
+        Returns:
+            self.swell_period (float): Swell period in seconds.
+        """
         return self.swell_period
 
     def get_wave_directions(self):
+        """
+        Returns the direction the waves are coming from in degrees. 0 degrees is North, increasing
+        clockwise.
+
+        Args:
+            None.
+        Returns:
+            self.swell_height (int): Swell direction in degrees.
+        """
         return self.swell_direction
 
 
 class Winds:
     def __init__(self):
+        """
+        Initializing the class automatically routes to the webpage for the Arecibo NOAA buoy data and pulls
+        the information from the webpage text relevant to wind speed, wind gust speed and direction.
+        NOTE: The buoy seems to not be collecting wind data at the moment, unfortunately. Which is why within the
+        url you can see these fields are marked with "MM". In this case, the value is set to the string
+        "Missing data from National Data Buoy Center" which is displayed on the webpage html.
+
+        Args:
+            None.
+        Returns:
+            None, but assigns the values from the webpage to variables within the Waves class.
+        """
 
         data = urlopen('https://www.ndbc.noaa.gov/data/realtime2/41121.txt')
 
@@ -96,18 +142,48 @@ class Winds:
         return 'Data list: ' + str(conditions)
 
     def get_wind_speed(self):
+        """
+        If wind data is found online, it returns a string with the wind speed and units. Else,
+        it returns the string "Missing data from National Data Buoy Center." to display on the webpage.
+
+        Args:
+            None.
+        Returns:
+            self.wind_speed (string): with either the units if a value is found or a message to the user if not.
+        """
+
         if isinstance(self.wind_speed, float):
             return str(self.wind_speed) + f'{self.wind_speed_units}'
         else:
             return self.wind_speed
 
     def get_gusts(self):
+        """
+        If wind data is found online, it returns a string with the wind gust speed and units. Else,
+        it returns the string "Missing data from National Data Buoy Center." to display on the webpage.
+
+        Args:
+            None.
+        Returns:
+            self.gusts (string): with either the units if a value is found or a message to the user if not.
+        """
+
         if isinstance(self.gusts, float):
             return str(self.gusts) + f'{self.gust_units}'
         else:
             return self.gusts
 
     def get_wind_direction(self):
+        """
+        If wind data is found online, it returns a string with the wind direction in degrees. Else,
+        it returns the string "Missing data from National Data Buoy Center." to display on the webpage.
+
+        Args:
+            None.
+        Returns:
+            self.wind_direction (string): with either the units if a value is found or a message to the user if not.
+        """
+
         if isinstance(self.wind_direction, int):
             return str(self.wind_direction) + f'{self.direction_units}'
         else:
